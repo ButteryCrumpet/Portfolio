530eeeec7d899c99956dc39d4170f0cecd467a70
Simon Leigh <sleigh564@hotmail.com>
2017-01-05 21:42:15 +0900

    added project model and implemented in template as well as gradual design building

--- portfolio/portHome/models.py
+++ portfolio/portHome/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+TYPE_CHOICES = (('Frontend','Frontend'), ('Backend','Backend'), ('Legacy','Legacy'), ('Coding','Coding'),)
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=TYPE_CHOICES)
+    languages = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- portfolio/portHome/views.py
+++ portfolio/portHome/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def home(request):
-    return render(request, 'portHome/home.html')
+    frontend_projects = Project.objects.filter(type='Frontend')
+    backend_projects = Project.objects.filter(type='Backend')
+    return render(request, 'portHome/home.html', {'frontend_projects': frontend_projects, 'backend_projects': backend_projects})
