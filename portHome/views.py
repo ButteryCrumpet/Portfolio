f3de1362ef943f5d959e5a412aaecbf4f4e03314
Simon Leigh <sleigh564@hotmail.com>
2017-01-20 11:30:12 +0900

    added about me view and refactored templates

--- portHome/views.py
+++ portHome/views.py
@@ -6,3 +6,6 @@ def home(request):
     frontend_projects = Project.objects.filter(type='Frontend')
     backend_projects = Project.objects.filter(type='Backend')
     return render(request, 'portHome/home.html', {'frontend_projects': frontend_projects, 'backend_projects': backend_projects})
+
+def about_me(request):
+    return render(request, 'portHome/about_me.html')
