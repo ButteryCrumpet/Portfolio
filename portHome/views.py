88e5b60e9a3844a7d58d23e30598470a118841b7
Simon Leigh <crumpetybumpety@gmail.com>
2017-01-21 16:28:45 +0900

    whitenoise config and template/static file fix

--- portHome/views.py
+++ portHome/views.py
@@ -5,11 +5,7 @@ from .models import Project
 def home(request):
     frontend_projects = Project.objects.filter(type='Frontend')
     backend_projects = Project.objects.filter(type='Backend')
-<<<<<<< HEAD
-    return render(request, 'porthome/home.html', {'frontend_projects': frontend_projects, 'backend_projects': backend_projects})
-=======
     return render(request, 'portHome/home.html', {'frontend_projects': frontend_projects, 'backend_projects': backend_projects})
 
 def about_me(request):
     return render(request, 'portHome/about_me.html')
->>>>>>> f3de1362ef943f5d959e5a412aaecbf4f4e03314
